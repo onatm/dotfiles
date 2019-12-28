89aa072b6fc9d9f21775309b249c40f09618c0b5
onatm <onatmercan@gmail.com>
2019-12-28 22:26:13 +0000

    Remove brew non-sense from fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ end
 
 set -gx EDITOR nvim
 
-set -x GOROOT /usr/local/opt/go/libexec
+# set -x GOROOT /usr/local/opt/go/libexec
 set -x GOPATH $HOME/dev/go
 set -x GO111MODULE on
 
@@ -22,34 +22,6 @@ set -gx FISH_KUBECTL_COMPLETION_COMPLETE_CRDS 0
 # macOS specific
 set PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
 set PATH /usr/local/opt/grep/libexec/gnubin $PATH
-
-set -g fish_user_paths "/usr/local/opt/libpq/bin" $fish_user_paths
-set -gx LDFLAGS "-L/usr/local/opt/libpq/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/libpq/include"
-
-set -g fish_user_paths "/usr/local/opt/binutils/bin" $fish_user_paths
-set -gx LDFLAGS "-L/usr/local/opt/binutils/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/binutils/include"
-
-set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
-set -gx LDFLAGS "-L/usr/local/opt/gettext/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/gettext/include"
-
-set -g fish_user_paths "/usr/local/opt/ncurses/bin" $fish_user_paths
-set -gx LDFLAGS "-L/usr/local/opt/ncurses/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/ncurses/include"
-
-set -g fish_user_paths "/usr/local/opt/m4/bin" $fish_user_paths
-set -gx LDFLAGS "-L/usr/local/opt/m4/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/m4/include"
-
-set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
-set -gx LDFLAGS "-L/usr/local/opt/openssl@1.1/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/openssl@1.1/include"
-
-set -g fish_user_paths "/usr/local/opt/avr-gcc@8/bin"
-set -gx LDFLAGS "-L/usr/local/opt/avr-gcc@8/lib"
-set -gx CLDFLAGS "-I/usr/local/opt/avr-gcc@8/include"
 
 # aliases
 alias v='nvim'
