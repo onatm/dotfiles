1bbaa683ce63394f32ff6f269aea8cc8cb3f8589
Onat Yiğit Mercan <onatmercan@gmail.com>
2025-03-27 22:22:26 +0000

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,49 +1,50 @@
 eval (starship init fish)
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-fish_vi_key_bindings
+mcfly init fish | source
 
 # Base16 Shell
-if status --is-interactive
+if status is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
     source "$BASE16_SHELL/profile_helper.fish"
 end
 
+fish_vi_key_bindings
+
 set -gx EDITOR nvim
 set -x GPG_TTY (tty)
+
+set -g fish_user_paths "/opt/homebrew/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
+
+set -g fish_user_paths "$GOPATH/bin" $fish_user_paths
+set -g fish_user_paths "$GOROOT/bin" $fish_user_paths
+
+set -g fish_user_paths "$HOME/.dotnet/tools" $fish_user_paths
+
+fish_add_path /opt/homebrew/opt/libpq/bin
+
+# kubectl
+set -gx FISH_KUBECTL_COMPLETION_COMPLETE_CRDS 0
+
+# mcfly
+set -gx MCFLY_KEY_SCHEME vim
 
 # bat
 set -x BAT_THEME base16
 
-set -x GOROOT /usr/local/opt/go/libexec
-set -x GOPATH $HOME/dev/go
-set -x GO111MODULE on
-
-set PATH $HOME/.cargo/bin $PATH
-set PATH $HOME/.linkerd2/bin $PATH
-set PATH $GOPATH/bin $PATH
-set PATH $GOROOT/bin $PATH
-set PATH $HOME/.dapr $PATH
-
-# kubectl
-set -gx PATH $PATH $HOME/.krew/bin
-set -gx FISH_KUBECTL_COMPLETION_COMPLETE_CRDS 0
-
-# rbenv
-status --is-interactive; and source (rbenv init -|psub)
-
-# macOS specific
-set PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
-set PATH /usr/local/opt/grep/libexec/gnubin $PATH
-set PATH /usr/local/opt/make/libexec/gnubin $PATH
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/mysql-client/bin" $fish_user_paths
-
 # aliases
 alias v='nvim'
-alias ls='exa'
+alias ls='eza'
 alias k='kubectl'
 alias kn='k ns'
 alias kall='k allctx'
+
+# docker aliases
+alias dcu='docker compose up -d'
+alias dcd='docker compose down'
 
 # git aliases
 alias gg='git graph'
